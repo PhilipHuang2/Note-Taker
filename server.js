e2ddb08480a7239cae2c19a6ee94235acdb0c23f
Philip Huang <philiphuang2@gmail.com>
2022-10-26 20:35:56 -0700

    created api/notes endpoint

--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const { nextTick } = require('process');
 
 const PORT = 3001;
 
@@ -10,7 +12,27 @@ const app = express();
 app.get("/", (req,res) =>
     res.sendFile(path.join(__dirname, 'public/index.html')));
 
+app.get("/notes", (req,res) =>
+    res.sendFile(path.join(__dirname, 'public/notes.html')));
 
+app.get("/api/notes", (req,res) =>
+    {
+        fs.readFile(path.join(__dirname,'db/db.json'), 'utf-8', (err,data)=>{
+            if(err)
+            {
+                console.log(err);
+                next(err);
+            }
+            else
+            {
+                const notes = JSON.parse(data);
+                res.json(notes)
+            }
+        })
+
+        // res.json() send db 
+    }
+)
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
