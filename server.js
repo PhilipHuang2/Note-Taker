f4843436201df43b02f9d9ae352bdad79e21b3f2
Philip Huang <philiphuang2@gmail.com>
2022-10-28 21:49:41 -0700

    added the delete endpoint

--- server.js
+++ server.js
@@ -19,6 +19,7 @@ app.get("/", (req,res) =>
 app.get("/notes", (req,res) =>
     res.sendFile(path.join(__dirname, 'public/notes.html')));
 
+app.get("/public/assets/images/notes")
 
 app.get("/api/notes", (req,res) =>
     {
@@ -30,14 +31,11 @@ app.get("/api/notes", (req,res) =>
             }
             else
             {
-                ;
                 res.json(JSON.parse(data));
             }
         })
     }
 )
-
-
 
 app.post("/api/notes", (req,res) => {
     let note = req.body;
@@ -54,11 +52,41 @@ app.post("/api/notes", (req,res) => {
             db.push(note);
             fs.writeFile(path.join(__dirname,"db/db.json"), JSON.stringify(db), err =>{
                 err ? console.log(err) : console.info(`\nData written to ${path.join(__dirname,"db/db.json")}`);
+                res.json(note);
+            });
+        }
+    }) 
+})
+
+app.delete("/api/notes/:id", (req,res)=>{
+    let id = req.params.id;
+    fs.readFile(path.join(__dirname,"db/db.json"), 'utf-8', (err,data)=>{
+        if(err) {
+            console.log(err);
+            next(err);
+        }
+        else {
+            // db is database object
+            const db = JSON.parse(data);
+            // need to iterate through Object array
+            // and delete the object with the same id
+            let pointer;
+            for(let i = 0; i < db.length; i++){
+                if(db[i].id == id){
+                    pointer = i;
+                }
+            }
+            // take the object at i out
+            db.splice(pointer,1);
+            fs.writeFile(path.join(__dirname,"db/db.json"), JSON.stringify(db), err =>{
+                err ? console.log(err) : console.info(`\nData deleted to ${path.join(__dirname,"db/db.json")}`);
+                res.json(db);
             });
         }
     }) 
 
-})
+});
+
 
 app.listen(PORT, () =>
     console.log(`App listening at http://localhost:${PORT} 🚀`)
